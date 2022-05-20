36257c944cca6e9f228c7e45bcc1c86055692fed
sanaz <86094601+sanaz-git@users.noreply.github.com>
2022-05-20 12:07:57 +0430

    fix

--- src/componenets/CityList.js
+++ src/componenets/CityList.js
@@ -4,15 +4,22 @@ import cityWeather from "../city-weather.json";
 const CityList = () => {
   return (
     <div className="city-list">
-      {cityWeather.map((city, index) => (
-        <div>
-          <h3 key={index}>
-            {city.name}, {city.sys.country}
-          </h3>
-
-          <span> temp: {Math.round(city.main.temp - 273)} &#8451; </span>
-        </div>
-      ))}
+      <ul>
+        {cityWeather.map((city, index) => (
+          <li key={index}>
+            <h2>
+              {city.name}, {city.sys.country}
+            </h2>
+            <h3> {city.weather[0].main} </h3>
+            <h4> {city.weather[0].description} </h4>
+            <p> min temp: {Math.round(city.main.temp_min - 273)} &#8451; </p>
+            <p> max temp: {Math.round(city.main.temp_max - 273)} &#8451; </p>
+            <p>
+              location: {city.coord.lat}, {city.coord.lon}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
